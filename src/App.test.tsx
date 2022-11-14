4e42c4518d30bd1e6bc8bccb4e12373029dc5dfc
Jhony Vega <jhonyvega.dev@gmail.com>
2022-11-14 08:10:39 -0500

    feat: implement markdown

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,149 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders text input', () => {
-  render(<App />);
-  const textarea = screen.getByRole('textbox');
-  expect(textarea).toBeInTheDocument();
+const getPreview = () => screen.getByTestId('preview');
+
+const getTextarea = () => screen.getByRole('textbox');
+
+const existsElements = (element: HTMLElement, tags: string[]) => {
+  tags.forEach((tag) => {
+    expect(element.innerHTML).toContain(`<${tag}>`);
+  });
+};
+
+describe('Markdown editor', () => {
+  test('renders text input', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    expect(textarea).toBeInTheDocument();
+  });
+
+  test('renders title in preview', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(textarea, `# markdown guide`);
+
+    const preview = getPreview();
+    expect(preview).toContainHTML('<h1>markdown guide</h1>');
+  });
+
+  test('renders subtitle in preview', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(textarea, `## markdown subtitle`);
+
+    const preview = getPreview();
+    expect(preview).toContainHTML('<h2>markdown subtitle</h2>');
+  });
+
+  test('renders lists in preview using the * symbol', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(
+      textarea,
+      `
+    * item 1
+    * item 2
+    `.trim(),
+    );
+
+    const preview = getPreview();
+    expect(preview.innerHTML).toContain('item 1');
+    expect(preview.innerHTML).toContain('item 2');
+    expect(preview).toContainHTML(`<ul><li>item 1</li><li>item 2</li></ul>`);
+  });
+
+  test('renders lists in preview using the - symbol', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(
+      textarea,
+      `
+    - item 1
+    - item 2
+    `.trim(),
+    );
+
+    const preview = getPreview();
+    expect(preview.innerHTML).toContain('item 1');
+    expect(preview.innerHTML).toContain('item 2');
+    expect(preview).toContainHTML(`<ul><li>item 1</li><li>item 2</li></ul>`);
+  });
+
+  test('renders blocks in preview', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(
+      textarea,
+      `
+    hello
+    second
+    `.trim(),
+    );
+
+    const preview = getPreview();
+    expect(preview).toHaveTextContent('hello');
+    expect(preview).toContainHTML(`<p>hello</p><p>second</p>`);
+  });
+
+  test('renders multiple title and blocks', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(
+      textarea,
+      `
+    # title
+    ## subtitle
+    message
+    `.trim(),
+    );
+
+    const preview = getPreview();
+    expect(preview).toHaveTextContent('title');
+    expect(preview).toHaveTextContent('subtitle');
+    expect(preview).toHaveTextContent('message');
+    expect(preview).toContainHTML(`<h1>title</h1><h2>subtitle</h2><p>message</p>`);
+  });
+
+  test('should update new markdown content by changing the input', () => {
+    render(<App />);
+    const textarea = getTextarea();
+    userEvent.type(
+      textarea,
+      `
+    # heading 1
+      this is a block
+    `.trim(),
+    );
+
+    let preview = getPreview();
+    expect(preview).toHaveTextContent('heading 1');
+    expect(preview).toHaveTextContent('this is a block');
+    existsElements(preview, ['h1', 'p']);
+
+    userEvent.clear(textarea);
+
+    preview = getPreview();
+    expect(preview).toBeEmptyDOMElement();
+
+    userEvent.type(
+      textarea,
+      `
+    ## heading 2 
+    * item 1
+    * item 2
+    `.trim(),
+      {},
+    );
+
+    preview = getPreview();
+    expect(preview).not.toHaveTextContent('heading 1');
+    expect(preview).not.toHaveTextContent('this is a block');
+    expect(preview).toHaveTextContent('heading 2');
+    expect(preview).toHaveTextContent('item 1');
+    expect(preview).toHaveTextContent('item 2');
+    existsElements(preview, ['h2', 'ul', 'li']);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,133 @@
+import { useState } from 'react';
+
 type Block = {
   element: 'ul' | 'li' | 'p' | 'h1' | 'h2';
   children: (Block | string)[];
 };
 
+type BlockElement = Block['element'];
+
+type MarkdownKey = '#' | '##' | '*' | '-' | '';
+
+type LexicalNode = ReturnType<typeof analyzeLexicalContent>[number];
+
+const ELEMENT = {
+  ul: 'ul',
+  li: 'li',
+  p: 'p',
+  h1: 'h1',
+  h2: 'h2',
+} as const;
+
+const MARKDOWN_KEY = {
+  '#': ELEMENT.h1,
+  '##': ELEMENT.h2,
+  '*': ELEMENT.li,
+  '-': ELEMENT.li,
+  '': ELEMENT.p,
+} as const;
+
+const id = () => String(Math.floor(Math.random() * Date.now()));
+
+const parseLine = (line: string = '') => {
+  let contentCleaned = line.trim();
+  const [firstWord, ...restWords] = contentCleaned.split(' ');
+  let element = MARKDOWN_KEY[firstWord as MarkdownKey];
+  let content = restWords.join(' ');
+  if (!element) {
+    content = contentCleaned;
+    element = MARKDOWN_KEY[''];
+  }
+  return {
+    element,
+    children: content,
+  };
+};
+
+const analyzeLexicalContent = (content: string) => {
+  const contentLines = content.split('\n').filter((line) => line !== '');
+
+  return contentLines.map((line, index) => {
+    const current = parseLine(line);
+    const previous = parseLine(contentLines[index - 1]);
+
+    return {
+      ...current,
+      is: (element: BlockElement) => current.element === element,
+      previousNode: {
+        ...previous,
+        isEqualElement: current.element === previous.element,
+      },
+    };
+  });
+};
+
+const createBlock = (node: LexicalNode): Block => ({
+  element: node.element,
+  children: [node.children],
+});
+
 const parseMarkdown = (markdown: string): Block[] => {
-  // TODO: implement
+  const parseElements = analyzeLexicalContent(markdown);
+  const newElements: Record<string, Block> = {};
+  const elementsIds: Record<string, string> = {};
+
+  parseElements.forEach((currentElement, index) => {
+    const { previousNode } = currentElement;
+
+    if (currentElement.is(ELEMENT.li)) {
+      elementsIds[index] = previousNode.isEqualElement ? elementsIds[index - 1] : id();
+      const elementId = elementsIds[index];
+      const hasRepeatedElementId = elementId === elementsIds[index - 1];
+      if (hasRepeatedElementId) {
+        newElements[elementId].children = [
+          ...newElements[elementId].children,
+          createBlock(currentElement),
+        ];
+      } else {
+        newElements[elementId] = {
+          element: ELEMENT.ul,
+          children: [createBlock(currentElement)],
+        };
+      }
+      return;
+    }
+
+    const uniqueId = id();
+    elementsIds[index] = uniqueId;
+    newElements[uniqueId] = createBlock(currentElement);
+  });
+
+  const elementsValue = Object.keys(newElements).map((el) => newElements[el]);
+  return elementsValue;
+};
+
+// Parse markdown in React components
+const marked = (markdown: string): JSX.Element[] => {
+  function renderNode(node: Block, index: number) {
+    const { element: Element, children } = node;
+    return (
+      <Element key={index}>
+        {children.map((subItem, index) => {
+          if (typeof subItem === 'string') return subItem;
+          return renderNode(subItem, index);
+        })}
+      </Element>
+    );
+  }
+
+  return parseMarkdown(markdown).map(renderNode);
 };
 
 function App() {
+  const [markdown, setMarkdown] = useState<string>('');
+
   return (
-    <div>
-      <textarea onChange={(e) => console.log(e.target.value)} rows={5} />
+    <div className="editor">
+      <textarea rows={5} className="editor__input" onChange={(e) => setMarkdown(e.target.value)} />
+      <div className="editor__output" data-testid="preview">
+        {marked(markdown)}
+      </div>
     </div>
   );
 }
